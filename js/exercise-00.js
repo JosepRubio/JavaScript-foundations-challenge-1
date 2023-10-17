7994f236ff578a6aec6da0a9b2850ed6d8113d89
josep <josep.rubio@mataro.epiaedu.cat>
2023-10-17 20:29:42 +0200

    exercice-00 and index complete

---
+++ js/exercise-00.js
@@ -0,0 +1,22 @@
+/* 
+   Create a constant variable of each of the following primitive types:
+   - String
+   - Number
+   - Boolean
+   - Null
+   - Undefined
+
+   Log out each variable to the terminal at the end.
+*/
+
+const String = 'string';
+const Number = 4;
+const Boolean = true;
+const Null = false;
+const Undefined = undefined;
+
+console.log(String);
+console.log(Number);
+console.log(Boolean);
+console.log(Null);
+console.log(Undefined);
