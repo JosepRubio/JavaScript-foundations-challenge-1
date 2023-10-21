7064efe91f693bb402f57e2eb27c530f55a20cbe
josep <josep.rubio@mataro.epiaedu.cat>
2023-10-21 19:18:51 +0200

    update exercice 00 and complete 01 -  02 - 03 - 04

---
+++ js/exercise-04.js
@@ -0,0 +1,20 @@
+/*
+    What would each of these lines of code do:
+
+    1. true && console.log("moo");
+     l'operador && verifica tots dos operands en aquest cas el primer operand es true
+     per tant executa el segon que es la instrucció i imprimeix "moo" 
+    
+     2. false && console.log("moo moo?");
+    En aquest cas el primer operador es false per tant ja no executara el segon i no s'imprimira res 
+
+    3. true || console.log("hello friend");
+    l'operand || verifica tots dos operands en aquest cas el primer operand es true per tant ja no avaluara el segon 
+    ja que al ser true ja no necesita comprovar el segon per tant no imprimira res per pantalla  
+
+    4. false || console.log("bye friend");
+    En aquest cas al ser false el primer operand pasara a evaluar el segon en aquest cas el segon es una instruccio per tant l'executara iho imprimira per pantalla 
+
+    Once you see the results, write in a comment why it works that way
+*/
+
