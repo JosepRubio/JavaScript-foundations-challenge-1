d15fd9d21ab0484926ecb1a37515168f797e1f6f
josep <josep.rubio@mataro.epiaedu.cat>
2023-10-31 19:33:57 +0100

    complete exercise conditionals 00-02

--- challenge_03_loops-conditionals/loops-conditionals/conditionals/exercise_01.js
+++ challenge_03_loops-conditionals/loops-conditionals/conditionals/exercise_01.js
@@ -7,3 +7,10 @@
 
     HINT: modulus operator ( % ) is your friend
 */
+
+let number = 5;
+if (number % 2 == 0) {
+  console.log("The number is even");
+} else {
+  console.log("The number is odd");
+}
--- challenge_03_loops-conditionals/loops-conditionals/conditionals/exercise_02.js
+++ challenge_03_loops-conditionals/loops-conditionals/conditionals/exercise_02.js
@@ -6,7 +6,7 @@
 */
 const age = 20;
 
-if (age < 3) {
+/*if (age < 3) {
   console.log("You're just a baby!");
 } else if (age >= 3) {
   console.log("You are in elementary school, kid.");
@@ -14,6 +14,18 @@ if (age < 3) {
   console.log("Nice, you're a teenager!");
 } else if (age >= 18) {
   console.log("Most places consider you an adult.");
+} else {
+  console.log("What? How did this happen!?");
+}*/
+
+if (age >= 18) {
+  console.log("Most places consider you an adult.");
+} else if (age >= 13) {
+  console.log("Nice, you're a teenager!");
+} else if (age >= 3) {
+  console.log("You are in elementary school, kid.");
+} else if (age < 3) {
+  console.log("You're just a baby!");
 } else {
   console.log("What? How did this happen!?");
 }
